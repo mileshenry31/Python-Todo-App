0bca4bbaec0c1055ce9420d916a723da16b99513
mileshenry31 <73675082+mileshenry31@users.noreply.github.com>
2022-03-24 22:55:21 -0700

    Added button

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import showinfo
 
 root = Tk()
 root.geometry('750x500')
 root.title('To Do App')
-
-
 frm = ttk.Frame(root, padding=10)
 frm.grid()
 
-ttk.Button(frm, text='Add a todo')
-if __name__ == '__main__':
-    root.mainloop()
+
+
+def add_clicked():
+    showinfo(
+        title='Information',
+        message='Button clicked'
+    )
+
+
+
+
+ttk.Button(frm, text='Add a todo', command=add_clicked)
+
+root.mainloop()
